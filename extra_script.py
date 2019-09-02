85317fa0836d4cd4f06e146c0ec3aa0083ee790f
csgregg <csgregg@users.noreply.github.com>
2019-09-01 20:42:06 -0500

    Clean up

--- extra_script.py
+++ extra_script.py
@@ -4,4 +4,4 @@ my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 # print(defines)
 
-env.Replace(PROGNAME="csg-esp8266-rota-v_%s" % defines.get("BUILD_TAG"))
+env.Replace(PROGNAME="csg-esp8266-rota-v%s" % defines.get("BUILD_TAG"))
