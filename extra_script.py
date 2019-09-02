f6d78140f3bfbc584bbc85b6adcc01817dc4a5b8
csgregg <csgregg@users.noreply.github.com>
2019-09-01 19:35:18 -0500

    custom bin

--- extra_script.py
+++ extra_script.py
@@ -4,4 +4,4 @@ my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 # print(defines)
 
-env.Replace(PROGNAME="csg-esp8266-rota-v_%s" % defines.get("VERSION"))
+env.Replace(PROGNAME="csg-esp8266-rota-v_%s" % defines.get("BUILD_TAG"))
