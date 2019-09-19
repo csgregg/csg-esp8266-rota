f6106e10b1503499898b3a8fd1e363780e688283
csgregg <csgregg@users.noreply.github.com>
2019-09-18 20:19:15 -0500

    Correct $PROJECT_DIR

--- extra_script.py
+++ extra_script.py
@@ -8,17 +8,18 @@ platform = env.PioPlatform()
 
 
 def deflate_www(sourceFolder,destFolder):
-    print('Deflating www files -')
-    for folder, subfolders, files in os.walk(sourceFolder):
+
+    print('Deflating www files in ' + env.subst("$PROJECT_DIR") + sourceFolder)
+    for folder, subfolders, files in os.walk(env.subst("$PROJECT_DIR") + sourceFolder):
         for file in files:
             print(os.path.join(folder, file))
             with open(os.path.join(folder, file), 'rb') as f_in:
-                with gzip.open(os.path.join(destFolder, file) + '.gz', 'wb') as f_out:
+                with gzip.open(os.path.join(env.subst("$PROJECT_DIR") + destFolder, file) + '.gz', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
     print('Deflating complete')
 
 
-env.AddPreAction("$BUILD_DIR/spiffs.bin", deflate_www(".\www",".\data"))
+env.AddPreAction("$BUILD_DIR/spiffs.bin", deflate_www("\www","\data"))
 
 
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
