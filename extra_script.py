a4646601599c9a97b533cbd23e010f4a9762aa4c
csgregg <csgregg@users.noreply.github.com>
2019-09-18 19:43:22 -0500

    Test add spiffs

--- extra_script.py
+++ extra_script.py
@@ -1,4 +1,25 @@
+import os
+import gzip
+import shutil
+
 Import("env")
+
+platform = env.PioPlatform()
+
+
+def deflate_www(sourceFolder,destFolder):
+    print('Deflating www files -')
+    for folder, subfolders, files in os.walk(sourceFolder):
+        for file in files:
+            print(os.path.join(folder, file))
+            with open(os.path.join(folder, file), 'rb') as f_in:
+                with gzip.open(os.path.join(destFolder, file) + '.gz', 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+    print('Deflating complete')
+
+
+env.AddPreAction("$BUILD_DIR/partitions.bin", deflate_www(".\www",".\data"))
+
 
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
