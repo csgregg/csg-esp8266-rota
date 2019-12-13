6fba84adde7f78c24ee77721a0c094569d799ca0
csgregg <chris@greggs.org>
2019-12-12 23:18:52 -0600

    webserver generic request handler

--- src/main.cpp
+++ src/main.cpp
@@ -15,45 +15,48 @@ WiFiClient client;
 ESP8266WebServer server(80);
 
 
-void filefont1()
-{
-  File file = SPIFFS.open("/font.woff.gz", "r"); 
-  size_t sent = server.streamFile(file, "application/font-woff");
-  sent = sent;
+
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+
+
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".html.gz")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".css.gz")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".js.gz")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".ico.gz")) return "image/x-icon";
+  else if (filename.endsWith(".woff")) return "application/font-woff";
+  else if (filename.endsWith(".woff.gz")) return "application/font-woff";
+  return "text/plain";
 }
 
+// send the right file to the client (if it exists)
+bool handleFileRead(String shortpath) {
 
-void fileindex()
-{
-  File file = SPIFFS.open("/index.html.gz", "r"); 
-  size_t sent = server.streamFile(file, "text/html");
-  sent = sent;
+  Serial.println("handleFileRead: " + shortpath);
+  if (shortpath.endsWith("/")) shortpath += "index.html";         // If a folder is requested, send the index file
+
+  shortpath.replace("fonts/glyphicons-halflings-regular", "font");
+
+  String path = shortpath + ".gz";
+
+  Serial.println(path);
+
+  String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    size_t sent = server.streamFile(file, contentType); // And send it to the client
+    file.close();       
+    Serial.println(sent);                                // Then close the file again
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false;                                         // If the file doesn't exist, return false
 }
-
-
-void bootstrap()
-{
-  File file = SPIFFS.open("/bootstrap.min.css.gz", "r"); 
-  size_t sent = server.streamFile(file, "text/css");
-  sent = sent;
-}
-
-
-void bootstrapmin()
-{
-  File file = SPIFFS.open("/bootstrap.min.js.gz", "r"); 
-  size_t sent = server.streamFile(file, "application/javascript");
-  sent = sent;
-}
-
-
-void jquerymin()
-{
-  File file = SPIFFS.open("/jquery-3.4.1.min.js.gz", "r"); 
-  size_t sent = server.streamFile(file, "application/javascript");
-  sent = sent;
-}
-
 
 
 
@@ -109,20 +112,14 @@ void setup() {
     LOG("WiFI Started: " + WiFi.localIP().toString());
 
     server.begin(); 
-    server.on("/", fileindex);
-    server.on("/index.html", fileindex);
-    server.on("/bootstrap.min.css", bootstrap);
-    server.on("bootstrap.min.css", bootstrap);
-    server.on("/bootstrap.min.js", bootstrapmin);
-    server.on("bootstrap.min.js", bootstrapmin);
-    server.on("/jquery-3.4.1.min.js", jquerymin);
-    server.on("jquery-3.4.1.min.js", jquerymin);
-    server.on("/fonts/glyphicons-halflings-regular.woff", filefont1);
-    server.on("fonts/glyphicons-halflings-regular.woff", filefont1);
+
+    server.onNotFound([]() {                              // If the client requests any URI
+      if (!handleFileRead(server.uri()))                  // send it if it exists
+        server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+    });
+
 
     SPIFFS.begin(); 
-
-
 
     updater.setup( device_getBuildFlag(flag_UPDATER_SERVICE), device_getBuildFlag(flag_UPDATER_REPO), device_getBuildFlag(flag_UPDATER_USER), device_getBuildFlag(flag_UPDATER_TOKEN), device_getBuildFlag(flag_DEVICE_CODE), device_getBuildFlag(flag_BUILD_TAG), device_getBuildFlag(flag_UPDATER_INTERVAL), device_getBuildFlag(flag_UPDATER_SKIP) );
     updater.begin( client );
