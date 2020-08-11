60b3b6fb9a72b7b5004a794e78f26b3f14614007
csgregg <csgregg@users.noreply.github.com>
2020-08-11 17:43:05 +0100

    debug

--- scripts/rename_bins_post.py
+++ scripts/rename_bins_post.py
@@ -15,38 +15,23 @@ def get_build_flag_value(flag_name):
 
 
 
-# Compress firmware using gzip for 'compressed OTA upload'
-def compressFirmware(source):
-
-    print("Compressing firmware: " + source)
-    os.system("gzip -9 -k " + source)
-
-    target_path = os.path.dirname(os.path.abspath(source))
-    print("Dir: " + target_path )
-    for dirfiles in os.listdir(target_path):
-        print(dirfiles)
-
-    if os.path.exists(source +'.bak'):
-        ORG_FIRMWARE_SIZE = os.stat(source + '.bak').st_size
-        GZ_FIRMWARE_SIZE = os.stat(source + '.gz').st_size
-
-        print("Compression reduced firmware size by {:.0f}% (was {} bytes, now {} bytes)".format((GZ_FIRMWARE_SIZE / ORG_FIRMWARE_SIZE) * 100, ORG_FIRMWARE_SIZE, GZ_FIRMWARE_SIZE))
-
-
-
 # Change file system image name and compress
 def prepareBINs(*args, **kwargs):
     print("Extra Script (Post): rename_bins_post.py")
 
-    fstarget = str(kwargs['target'][0])
+    fstarget = str(kwargs['fstarget'][0])
     target_path = os.path.dirname(os.path.abspath(target))
     progtarget = os.path.join(target_path, env['PROGNAME']+".bin")
 
     new_fstarget = "%s-Fv%s.bin" % (get_build_flag_value("DEVICE_CODE"), get_build_flag_value("BUILD_TAG"))
     os.rename(fstarget, os.path.join(target_path, new_fstarget))
 
-    compressFirmware(progtarget)
-    compressFirmware(new_fstarget)
+    os.system("gzip -9 -k " + progtarget)
+    os.system("gzip -9 -k " + new_fstarget)
+
+    print("Dir: " + target_path )
+    for dirfiles in os.listdir(target_path):
+        print(dirfiles)
 
 
 
