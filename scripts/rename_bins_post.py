fee8027209765a5b19402a3c80330fdf68deab3f
csgregg <csgregg@users.noreply.github.com>
2020-08-11 14:45:44 +0100

    here

--- scripts/rename_bins_post.py
+++ scripts/rename_bins_post.py
@@ -21,6 +21,7 @@ def compressFirmware(source):
     if not os.path.exists(source +'.bak'):
         print("Compressing firmware: " + source)
         shutil.move(source, source + '.bak')
+        print("here")
         with open(source + '.bak', 'rb') as f_in:
             with gzip.open(source + '.gz', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
