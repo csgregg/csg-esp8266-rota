211f6738d1e758d7caece2e45603c761b0f018ef
csgregg <csgregg@users.noreply.github.com>
2020-08-14 23:36:11 +0100

    clean up

--- www/menu.js
+++ www/menu.js
@@ -1,14 +1,21 @@
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-    // Get the Sidebar
+function highlight_menu() {
     var mySidebar = document.getElementById("mySidebar");
-
     // Highlight menu item
     var url = window.location.pathname;
     var hrefname = "a[href='" + url.substring(url.lastIndexOf('/')+1) + "']";   
     if( hrefname == "a[href='']" ) hrefname = "a[href='index.html']";     // Catch error if / 
     var menuitem = mySidebar.querySelectorAll(hrefname);
     menuitem[0].classList.add("w3-blue");
+}
+
+
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    // Get the Sidebar
+    var mySidebar = document.getElementById("mySidebar");
+
+    highlight_menu();
 
     // Get the DIV with overlay effect
     var overlayBg = document.getElementById("myOverlay");
