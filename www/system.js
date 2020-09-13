e9aa645d668baabeaac0c10cccc67056495b4216
csgregg <csgregg@users.noreply.github.com>
2020-09-13 22:32:47 +0100

    Added web cofig for logger

--- www/system.js
+++ www/system.js
@@ -31,7 +31,30 @@ function SureYes() {
     document.getElementById('sure_dlg').style.display='none';
     document.getElementById('loader').style.display='block';
 
-    doRequest(sureAction,1,reloadPage);
+    if( sureAction == "log_save" ) {
+
+        x = document.getElementById('log_srl');
+        doRequest(x.id, x.checked ? 't' : 'f');
+        x = document.getElementById('log_baud');
+        doRequest(x.id, x.value);
+        x = document.getElementById('log_ser');
+        doRequest(x.id, x.checked ? 't' : 'f');
+        x = document.getElementById('log_url');
+        doRequest(x.id, x.value);
+        x = document.getElementById('log_key');
+        doRequest(x.id, x.value);
+        x = document.getElementById('log_tick');
+        doRequest(x.id, x.checked ? 't' : 'f');
+        x = document.getElementById('log_tick_int');
+        doRequest(x.id, x.value);
+        x = document.getElementById('log_tags');
+        doRequest(x.id, x.value);
+        x = document.getElementById('log_level');
+        doRequest(x.id, x.value);
+
+        doRequest(sureAction,1,clearLoader);
+    }
+    else doRequest(sureAction,1,reloadPage);
 
 }
 
