939bb2ef662e84143b5a516b8ecba115829d9824
csgregg <csgregg@users.noreply.github.com>
2020-09-18 17:06:20 +0100

    int checker settings configurable and cleanup

--- www/system.js
+++ www/system.js
@@ -26,23 +26,23 @@ function SureYes() {
     if( sureAction == "log_save" ) {
 
         x = document.getElementById('log_srl');
-        doRequest(x.id, x.checked ? 't' : 'f');
+        doRequestWait(x.id, x.checked ? 't' : 'f');
         x = document.getElementById('log_baud');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
         x = document.getElementById('log_ser');
-        doRequest(x.id, x.checked ? 't' : 'f');
+        doRequestWait(x.id, x.checked ? 't' : 'f');
         x = document.getElementById('log_url');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
         x = document.getElementById('log_key');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
         x = document.getElementById('log_tick');
-        doRequest(x.id, x.checked ? 't' : 'f');
+        doRequestWait(x.id, x.checked ? 't' : 'f');
         x = document.getElementById('log_tick_int');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
         x = document.getElementById('log_tags');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
         x = document.getElementById('log_level');
-        doRequest(x.id, x.value);
+        doRequestWait(x.id, x.value);
 
         doRequest(sureAction,1,clearLoader);
     }
