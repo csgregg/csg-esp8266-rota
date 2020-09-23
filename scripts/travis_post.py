b4c2fbaaf758b30f899947f3cbde334bc434e64b
Chris Gregg <chris@greggs.org>
2020-09-23 10:58:12 +0100

    debug

--- scripts/travis_post.py
+++ scripts/travis_post.py
@@ -13,42 +13,32 @@ def get_build_flag_value(flag_name):
 
 
 # Change file system image name and compress
-def prepareBINs(*args, **kwargs):
+def gzipBINs(*args, **kwargs):
     print("Extra Script (Post): process_bins_post.py")
 
     fstarget = str(kwargs['target'][0])
     target_path = os.path.dirname(os.path.abspath(fstarget))
     progtarget = os.path.join(target_path, env['PROGNAME']+".bin")
 
-    new_fstarget = os.path.join(target_path, "%s-Fv%s.bin" % (get_build_flag_value("DEVICE_CODE"), get_build_flag_value("BUILD_TAG")))
-
+    print(fstarget)
     print(target_path)
-    os.system("ls -las " + target_path)
+    print(progtarget) 
 
-    os.rename(fstarget, new_fstarget)
-
-    os.system("ls -las " + target_path)
-    
     os.system("gzip -9 -k " + progtarget)
-    os.system("gzip -9 -k " + new_fstarget)
+    os.system("gzip -9 -k " + fstarget)
 
     os.system("ls -las " + target_path)
-    os.system("ls -las /tmp")
 
-def tempfunc(*args, **kwargs):
+
+def keepFSBIN(*args, **kwargs):
     fstarget = str(kwargs['target'][0])
     target_path = os.path.dirname(os.path.abspath(fstarget))
-    progtarget = os.path.join(target_path, env['PROGNAME']+".bin")
     new_fstarget = os.path.join(target_path, "%s-Fv%s.bin" % (get_build_flag_value("DEVICE_CODE"), get_build_flag_value("BUILD_TAG")))
-    print(target_path)
-    print(progtarget)
-    print(new_fstarget)
-    os.system("ls -las " + target_path)
-    os.system("ls -las /tmp")
-    os.system("mv " + fstarget + " /tmp")
-    os.system("ls -las /tmp")
+
+    os.rename(fstarget, new_fstarget)
+    os.system("mv " + new_fstarget + " /tmp")
 
 
 # Set up call after FS bin has been created
-env.AddPostAction("$BUILD_DIR/littlefs.bin", tempfunc) 
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", prepareBINs) 
+env.AddPostAction("$BUILD_DIR/littlefs.bin", keepFSBIN) 
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", gzipBINs) 
