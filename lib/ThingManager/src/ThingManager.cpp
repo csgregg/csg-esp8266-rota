18395c2557b03d7e38768568ccd966f174fbf0ad
csgregg <csgregg@users.noreply.github.com>
2022-01-22 22:49:51 +0000

    Thinger logging added

--- lib/ThingManager/src/ThingManager.cpp
+++ lib/ThingManager/src/ThingManager.cpp
@@ -92,6 +92,52 @@ void ICACHE_FLASH_ATTR ThingManager::Restart( ThingerSettings& settings ){
 
         if( NULL == io ){
             io = new ThingerClient(*_client, _settings->user, _settings->device, _settings->token);
+
+
+            io->set_state_listener([&](ThingerClient::THINGER_STATE state){
+                switch(state){
+                    case ThingerClient::NETWORK_CONNECTING:
+                        LOG_HIGH(F("(Thinger) Network connecting"));
+                        break;
+                    case ThingerClient::NETWORK_CONNECTED:
+                        LOG_HIGH(F("(Thinger) Network connected"));
+                        break;
+                    case ThingerClient::NETWORK_CONNECT_ERROR:
+                        LOG_HIGH(F("(Thinger) Network connection error"));
+                        break;
+                    case ThingerClient::SOCKET_CONNECTING:
+                        LOG_HIGH(F("(Thinger) Socket connecting"));
+                        break;
+                    case ThingerClient::SOCKET_CONNECTED:
+                        LOG_HIGH(F("(Thinger) Socket connected"));
+                        break;
+                    case ThingerClient::SOCKET_CONNECTION_ERROR:
+                        LOG_HIGH(F("(Thinger) Socket connection error"));
+                        break;
+                    case ThingerClient::SOCKET_DISCONNECTED:
+                        LOG_HIGH(F("(Thinger) Socket disconnected"));
+                        break;
+                    case ThingerClient::SOCKET_ERROR:
+                        LOG_HIGH(F("(Thinger) Socket error"));
+                        break;
+                    case ThingerClient::SOCKET_TIMEOUT:
+                        LOG_HIGH(F("(Thinger) Socket timeout"));
+                        break;
+                    case ThingerClient::THINGER_AUTHENTICATING:
+                        LOG_HIGH(F("(Thinger) Authenticating"));
+                        break;
+                    case ThingerClient::THINGER_AUTHENTICATED:
+                        LOG_HIGH(F("(Thinger) Authenticated"));
+                        break;
+                    case ThingerClient::THINGER_AUTH_FAILED:
+                        LOG_HIGH(F("(Thinger) Authentication failed"));
+                        break;
+                    case ThingerClient::THINGER_STOP_REQUEST:
+                        LOG_HIGH(F("(Thinger) Stop request"));
+                        break;
+                }
+            });
+
             
             (*thing.io)["led"] << [](pson& in){
                 if(in.is_empty()){
