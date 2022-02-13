582f09dfe3292c5ca67692812b71b3382f9551a4
csgregg <csgregg@users.noreply.github.com>
2022-02-13 17:25:17 +0000

    again

--- public_html/tools/deploy.php
+++ public_html/tools/deploy.php
@@ -1,7 +1,9 @@
 
 <?php
 
-    shell_exec('cd && pwd');
+    $output = shell_exec('cd && pwd');
+    echo "<pre>$output</pre>";
+
     $output = shell_exec('uapi VersionControlDeployment create repository_root=csg-esp8266-rota');
     echo "<pre>$output</pre>";
   
