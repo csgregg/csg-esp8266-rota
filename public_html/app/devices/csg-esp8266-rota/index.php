c2eb898da378e9b4181bbe120846bba19d7b61a5
Chris Gregg <chris@greggs.org>
2022-03-11 14:23:43 +0000

    some spice

--- public_html/app/devices/csg-esp8266-rota/index.php
+++ public_html/app/devices/csg-esp8266-rota/index.php
@@ -1,3 +1,9 @@
 <?php
+    require_once '../users/init.php';
+    require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+    if (!securePage($_SERVER['PHP_SELF'])){die();}
+?>
+
+<?php
 echo "This is csg-esp8266-rota";
 ?>
