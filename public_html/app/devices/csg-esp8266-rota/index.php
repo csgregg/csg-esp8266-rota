a88c651182dfc4c9ea4ad74ecddb82d713e26a81
Chris Gregg <chris@greggs.org>
2022-03-11 14:24:36 +0000

    fix

--- public_html/app/devices/csg-esp8266-rota/index.php
+++ public_html/app/devices/csg-esp8266-rota/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../users/init.php';
+    require_once '../../../users/init.php';
     require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
     if (!securePage($_SERVER['PHP_SELF'])){die();}
 ?>
